f303a3c3994aaae4e538584998130ec769a3b09a
TemoElizbarashvili <115801621+TemoElizbarashvili@users.noreply.github.com>
2023-10-19 17:09:38 +0400

    half done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { AboutCarComponent } from './components/about-car/about-car.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    NavigationComponent,
+    AboutCarComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/about-car/about-car.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-car',
+  templateUrl: './about-car.component.html',
+  styleUrls: ['./about-car.component.scss']
+})
+export class AboutCarComponent {
+
+}
