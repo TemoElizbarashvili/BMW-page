6b989f6c6472795fc575b735b7f9a1b9d0d350c1
TemoElizbarashvili <115801621+TemoElizbarashvili@users.noreply.github.com>
2023-10-20 18:39:34 +0400

    finished project
    project is done with fully responsive layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'BMW';
+
+  ngOnInit(): void {
+    window.addEventListener("scroll", this.reveal);
+  }
+
+  reveal() {
+    const elements = document.querySelectorAll('.fade');
+
+    for (var i = 0; i < elements.length; i++) {
+      let elementTop = elements[i].getBoundingClientRect().top;
+      let windowHeight = window.innerHeight;
+      let elementVisible = 250;
+
+      if (elementTop < windowHeight - elementVisible) {
+        elements[i].classList.add('animate');
+      } else {
+        elements[i].classList.remove('animate');
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { AboutCarComponent } from './components/about-car/about-car.component';
 import { FeaturesComponent } from './components/features/features.component';
+import { SpecsComponent } from './components/specs/specs.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,14 @@ import { FeaturesComponent } from './components/features/features.component';
     IntroComponent,
     NavigationComponent,
     AboutCarComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    SpecsComponent,
+    GalleryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
